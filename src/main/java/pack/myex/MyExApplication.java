8ed071cc6440c5fd2d7c740f2f037cfe0c8e1561
2021301041-crypto <2021301041@skuniv.ac.kr>
2026-03-31 15:00:23 +0900

    15:00 start

---
+++ src/main/java/pack/myex/MyExApplication.java
@@ -0,0 +1,21 @@
+package pack.myex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MyExApplication {
+
+    @GetMapping("/")
+    public String hello() {
+        System.out.println("MyEx");
+        return "MyEx Test: Am 14:43";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyExApplication.class, args);
+    }
+}
---
+++ src/test/java/pack/myex/MyExApplicationTests.java
@@ -0,0 +1,13 @@
+package pack.myex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
