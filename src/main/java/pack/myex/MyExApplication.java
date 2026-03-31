2c9e218522ef6479388d68e89ae48414548cdb1f
2021301041-crypto <2021301041@skuniv.ac.kr>
2026-03-31 16:53:00 +0900

    16:52

--- src/main/java/pack/myex/MyExApplication.java
+++ src/main/java/pack/myex/MyExApplication.java
@@ -12,7 +12,7 @@ public class MyExApplication {
     @GetMapping("/")
     public String hello() {
         System.out.println("MyEx");
-        return "MyEx Test: Am 16:43";
+        return "MyEx Test: Am 16:52";
     }
 
     public static void main(String[] args) {
